d2476a513f1ef8af8f9edfb5221bde0e0db539bc
leepuppychow <leepuppychow@gmail.com>
2017-12-05 17:01:49 -0700

    trip model tests complete

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -111,17 +111,17 @@ describe Trip do
 
   describe "Class Methods" do
     before(:each) do
-      @trip1 = Trip.create(duration: 35, start_date: DateTime.now,
+      @trip1 = Trip.create(duration: 35, start_date: "12/5/2017",
         start_station_name: "Japan", start_station_id: 5,
         end_date: DateTime.now, end_station_name: "Narita",
         end_station_id: 7, bike_id: 105, subscription_type: "Subscriber")
 
-      @trip2 = Trip.create(duration: 40, start_date: DateTime.now,
+      @trip2 = Trip.create(duration: 40, start_date: "12/5/2017",
         start_station_name: "Japan", start_station_id: 5,
         end_date: DateTime.now, end_station_name: "Narita",
         end_station_id: 7, bike_id: 105, subscription_type: "Customer")
 
-      @trip3 = Trip.create(duration: 45, start_date: DateTime.now,
+      @trip3 = Trip.create(duration: 45, start_date: "29/5/2003",
         start_station_name: "Japan", start_station_id: 5,
         end_date: DateTime.now, end_station_name: "Narita",
         end_station_id: 7, bike_id: 37, subscription_type: "Subscriber")
@@ -140,11 +140,13 @@ describe Trip do
     end
 
     it "can calculate the total rides per month" do
-      expect(Trip.total_rides_per_month).to eq(3)
+      expected = {"May 2017"=>2, "May 2003"=>1}
+      expect(Trip.total_rides_per_month).to eq(expected)
     end
 
     it "can calculate the total rides per year" do
-      expect(Trip.total_rides_per_year).to eq(3)
+      expected = {2017=>2, 2003=>1}
+      expect(Trip.total_rides_per_year).to eq(expected)
     end
 
     it "can find the most ridden bike" do
@@ -169,9 +171,38 @@ describe Trip do
       expect(Trip.percentage_subscription_type).to eq expected
     end
 
-  end
+    it "can find the date with the most trips" do
+      expect(Trip.date_with_highest_trips).to eq("12/5/2017")
+    end
 
-  describe "Instance Methods" do
+    it "can find the date with the fewest trips" do
+      expect(Trip.date_with_lowest_trips).to eq("5/29/2003")
+    end
+
+    it "can find the weather conditions on the date with the most rides" do
+      weather_1 = Condition.create(date: "12/5/2017",
+                        max_temperature_f: 70.0,
+                        mean_temperature_f: 65.0,
+                        min_temperature_f: 63.0,
+                        mean_humidity: 50.0,
+                        mean_visibility_miles: 3,
+                        mean_wind_speed_mph: 5,
+                        precipitation_inches: 1)
+      expect(Trip.weather_on_day_with_most_rides).to eq weather_1
+    end
+
+    it "can find the weather conditions on the date with the fewest rides" do
+      weather_2 = Condition.create(date: "29/5/2003",
+                        max_temperature_f: 70.0,
+                        mean_temperature_f: 65.0,
+                        min_temperature_f: 63.0,
+                        mean_humidity: 50.0,
+                        mean_visibility_miles: 3,
+                        mean_wind_speed_mph: 5,
+                        precipitation_inches: 1)
+      expect(Trip.weather_on_day_with_least_rides).to eq weather_2
+    end
+
   end
 
 end
