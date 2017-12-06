147b3f7a00f625bd0f61c60e2b1c8eff1f066f89
leepuppychow <leepuppychow@gmail.com>
2017-12-06 09:49:30 -0700

    finished avg num of rides in temp range

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204214948) do
+ActiveRecord::Schema.define(version: 20171206033941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20171204214948) do
     t.integer "end_station_id"
     t.integer "bike_id"
     t.text "subscription_type"
-    t.integer "zip_code"
+    t.text "zip_code"
   end
 
 end
